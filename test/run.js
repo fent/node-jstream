d44c31fd0d743d19ee4a7b428f0dd378f53a9e2c
fent <933490+fent@users.noreply.github.com>
2012-07-20 19:42:33 -0700

    cleaning up some things

---
+++ test/run.js
@@ -0,0 +1,36 @@
+var JStream = require('..')
+  , assert = require('assert')
+  , fs = require('fs')
+  ;
+
+
+/**
+ * Tests that a `file` emits `expected` results given a `path`
+ *
+ * @param (string) description
+ * @param (string) file
+ * @param (Array.Object) expected
+ * @param (Array.Object) path
+ */
+module.exports = function runTest(description, file, expected, path) {
+  describe(description, function() {
+    it('JStream emits expected Javascript objects', function(done) {
+      var rs = fs.createReadStream(file);
+      var jstream = new JStream(path);
+      rs.pipe(jstream);
+
+      var dataEmitted = false;
+      var n = 0;
+
+      jstream.on('data', function(obj) {
+        dataEmitted = true;
+        assert.deepEqual(obj, expected[n++]);
+      });
+
+      rs.on('end', function() {
+        assert.ok(dataEmitted);
+        done();
+      });
+    });
+  });
+}
